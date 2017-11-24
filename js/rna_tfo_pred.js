4262df4269f015c894f8c892da154f72c4668129
guillemportella <guillemportella@gmail.com>
2017-11-24 16:44:16 +0000

    Testing reading files

--- js/rna_tfo_pred.js
+++ js/rna_tfo_pred.js
@@ -479,3 +479,18 @@ document.addEventListener("DOMContentLoaded",
 			});
 	}
 );
+document.addEventListener("DOMContentLoaded",
+	function(event) {
+       document.getElementById('file').addEventListener('change', readFile, false);
+
+       function readFile (evt) {
+           var files = evt.target.files;
+           var file = files[0];           
+           var reader = new FileReader();
+           reader.onload = function(event) {
+             console.log(event.target.result);            
+           }
+           reader.readAsText(file)
+        }
+	}
+);
